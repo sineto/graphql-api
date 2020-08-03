11fc4bbee079db9823f5de3055b32366eddf8b6e
Sinésio Neto <sn.sineto@gmail.com>
2020-08-03 18:07:12 -0300

    feat: implement CRUD postgre service

---
+++ src/services/pg/product.service.js
@@ -0,0 +1,52 @@
+const { ApolloError } = require('apollo-server-express');
+
+const init = (db) => {
+  const findImages = async (rows) => { 
+    const prodIds = rows.map((prod) => prod.id).join(',');
+    const { rows: imagesRes } = await db.query(`select * from images where product_id in (${prodIds})`);
+
+    const products = rows.map((prod) => {
+      return {
+        ...prod,
+        images: imagesRes.filter((img) => img.product_id === prod.id)
+      };
+    });
+    
+    return products;
+  };
+
+  const findAll = async ({ limit = 10, offset = 0, orderBy = 'id', order = 'ASC' } = {}) => {
+    const { rows } = await db.query(`select * from products order by ${orderBy} ${order} limit ${limit + 1} offset ${offset * limit}`);
+    if (!rows[0]) {
+      throw new ApolloError('Products not found', 404);
+    }
+
+    const products = await findImages(rows);
+
+    const hasNext = rows.length > limit;
+    if (hasNext) {
+      products.pop();
+    }
+    
+    return {
+      data: products,
+      hasNext
+    };
+  };
+
+  const create = async (data) => {
+    const { rows } = await db.query('insert into products (name, price) values ($1, $2) returning *', data);
+    if (!rows[0]) {
+      throw new ApolloError('Failed to create product', 500);
+    }
+
+    return rows[0];
+  };
+
+  return {
+    findAll,
+    create,
+  };
+};
+
+module.exports = init;
